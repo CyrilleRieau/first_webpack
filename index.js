d0d6089657767ed204709860e979cbf81aa86561
Cyrille <cyrille.rieau@gmail.com>
2017-08-17 12:28:15 +0200

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+    $('#para').css({
+        color: 'red',
+        backgroundColor: 'blue',
+        fontSize: 15
+    })
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: {
+        index: './index.js'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map'
+};
